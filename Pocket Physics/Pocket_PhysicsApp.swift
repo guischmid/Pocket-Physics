869c6b6566bfaf1c82678cc771975c46cd7bcaf3
Gigi <github.com.doorbell592@passmail.net>
2025-06-10 14:08:45 +0200

    Initial Commit

---
+++ Pocket Physics/Pocket_PhysicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_PhysicsApp.swift
+//  Pocket Physics
+//
+//  Created by Guilherme on 10.06.25.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_PhysicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pocket PhysicsTests/Pocket_PhysicsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_PhysicsTests.swift
+//  Pocket PhysicsTests
+//
+//  Created by Guilherme on 10.06.25.
+//
+
+import Testing
+@testable import Pocket_Physics
+
+struct Pocket_PhysicsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
